e3bc3e0bd64bbe09ae33c68340a9a0e749e4e76c
Olivia Shen <52755071+LivsWorld@users.noreply.github.com>
2019-07-23 15:45:15 -0700

    Add files via upload

--- relax_script.js
+++ relax_script.js
@@ -1,19 +1,17 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function animalFunction() {
+  var x = document.getElementById("animalPics");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function landscapesFunction() {
+  var x = document.getElementById("landscapes");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
   }
 }
