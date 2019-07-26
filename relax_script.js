2e410589a67e25fb488b830116d01ff642a8852f
Olivia Shen <52755071+LivsWorld@users.noreply.github.com>
2019-07-26 09:26:49 -0700

    Add files via upload

--- relax_script.js
+++ relax_script.js
@@ -15,3 +15,21 @@ function landscapesFunction() {
     x.style.display = "none";
   }
 }
+
+function calming_gifFunction() {
+  var x = document.getElementById("calming_gifs");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function poemFunction() {
+  var x = document.getElementById("poems");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
